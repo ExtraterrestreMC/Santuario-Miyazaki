e8aa73897d1e2c90d4b79819ab6f8636fedff0de
Alejandro Montero <97790473+ExtraterrestreMC@users.noreply.github.com>
2023-03-23 17:26:30 +0100

    Update index.js
    Se a creado sessionOption, CorsOtions, whitelist, CookieParse, Importados los modulos:JWT, express-session. archivos. importados: jwt.mw, auth.mw, Logger

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,8 +7,58 @@ const cors = require("cors");
 const AppError = require("./AppError");
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors());
+const logger = require("./Logs/logger");
 
+
+
+//CORS
+const whitelist = ["http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://127.0.0.1:5502", "http://127.0.0.1:5503", "http://127.0.0.1:5504", "http://127.0.0.1:5505", "http://127.0.0.1:5506"]
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            logger.access.info("Acceso a la back desde " + origin);
+            callback(null, true);
+        } else {
+            logger.error.fatal("Intento de acceso a la aplicación desde origen desautorizado: " + origin)
+            callback(null, false);
+        }
+    },
+    credentials: true
+}
+
+app.use(cors(corsOptions));
+app.use(cookieParser("uMfgWE8FA$@&H9LW"))
+
+
+//Para las sessiones
+const session = require("express-session");
+const jwtMW = require("./middleware/jwt.mw");
+const authorization = require("./middleware/auth.mw");
+
+const sessionOptions = {
+    secret: "iTF%g69M2D#w$hc&",
+    cookie: {
+        secure: true,
+        sameSite: "none",
+        maxAge: 60 * 60 * 24 * 1000 // == 24 h
+    }
+}
+app.use(session(sessionOptions));
+
+app.use((req, res, next) => {
+    if (
+        (req.url != "/api/v1/usuarios/autenticar" && req.url != "/api/v1/usuarios/autenticar/") &&
+        ((req.url != "/api/v1/usuarios" && req.url != "/api/v1/usuarios/"))
+    ) {
+        jwtMW.requireJWT(req, res, next)
+    } else {
+        if (req.method == "POST") {
+            next();
+        } else {
+            jwtMW.requireJWT(req, res, next);
+        }
+    }
+});
 
 
 //const basicRoutes = require("./routers/X.routes")
@@ -24,6 +74,8 @@ app.use(`/api/${version}/menu`, platosRoutes);
 //Para las vistas
 // app.set("views", path.join(__dirname, "views"))
 // app.set("view engine", "ejs")
+
+
 
 
 app.use((err, req, res, next) => {
